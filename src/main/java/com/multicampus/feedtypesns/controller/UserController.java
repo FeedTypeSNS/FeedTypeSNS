ad18436f0835fff1a9b95cf37446ac031ae52d02
Majiwoo <majiou4420@naver.com>
2023-04-21 17:14:02 +0900

    test commit

--- src/main/java/com/multicampus/feedtypesns/controller/UserController.java
+++ src/main/java/com/multicampus/feedtypesns/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.multicampus.feedtypesns.controller;
 
+import com.multicampus.feedtypesns.domain.Users.Users;
+import com.multicampus.feedtypesns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/new")
+    public String createForm(){
+        return "users/createUser";
+    }
+
+    @PostMapping("/users/new")
+    public String create(@RequestBody Users user, Model model){
+        userService.reg(user);
+        model.addAttribute("userId", user.getUserName());
+        model.addAttribute("nickname", user.getNickname());
+
+        return "redirect:/users/new";
+    }
+    //@RequestParam("userId") String userId, @RequestParam("nickname") String nickname
+
+
 }
