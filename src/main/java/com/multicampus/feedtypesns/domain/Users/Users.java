3768db6f0e6ce4439d52fcff9e1af9a3bb244195
areumh01 <areumh01@gmail.com>
2023-04-23 11:09:12 +0900

    기본 User Entity생성

--- src/main/java/com/multicampus/feedtypesns/domain/Users/Users.java
+++ src/main/java/com/multicampus/feedtypesns/domain/Users/Users.java
@@ -1,4 +1,22 @@
 package com.multicampus.feedtypesns.domain.Users;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
 }
